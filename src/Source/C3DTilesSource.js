c566d6d232cc2195059bcc39be15de79e3b983b1
mgermerie <73115044+mgermerie@users.noreply.github.com>
2021-11-23 11:19:13 +0100

    add doc for 3DTilesSources

--- src/Source/C3DTilesSource.js
+++ src/Source/C3DTilesSource.js
@@ -1,7 +1,18 @@
 import Source from 'Source/Source';
 import Fetcher from 'Provider/Fetcher';
 
+/**
+ * Define the source for 3d-tiles data.
+ *
+ * @class C3DTilesSource
+ */
 class C3DTilesSource extends Source {
+    /**
+     * Builds a new instance of C3DTilesSource
+     *
+     * @param {Object} source - An object that can contain all properties of a
+     * Source. Only the `url` property is mandatory.
+     */
     constructor(source) {
         super(source);
         this.baseUrl = this.url.slice(0, this.url.lastIndexOf('/') + 1);
